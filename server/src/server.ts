3de19e93e418e113fa8804a002478db855eaaed5
rjrocha19 <rodolfo_rj@live.com>
2023-05-16 16:41:44 -0300

    inicio do projeto

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,14 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 const app = fastify()
+const prisma = new PrismaClient()
+
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+
+  return users
+})
 
 app
   .listen({
